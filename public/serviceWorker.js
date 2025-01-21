a046dbb55b9cafd6b8281d6b6ba795000cbf3cfe
Sina Samadi <s.sina7u.ss@gmail.com>
2025-01-22 02:17:43 +0330

    seconds

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -34,7 +34,7 @@ registerRoute(
 
     return true;
   },
-  createHandlerBoundToURL(process.env.PUBLIC_URL + '/React-Site-With-Tailwind/index.html')
+  createHandlerBoundToURL('/React-Site-With-Tailwind/index.html')
 );
 
 // An example runtime caching route for requests that aren't handled by the
